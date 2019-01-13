e1e767da71a496119915f44c78205bdb8f7556e6
workspace <workspace@venkatesh.local>
2019-01-13 06:51:50 +0530

    added the register form validation

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const db = require('./config/keys').MondoURI;
 
 // Views
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Body parser
+app.use(express.urlencoded({extended: false}));
+
+// Mongodb connection
+mongoose.connect(db, {useNewUrlParser: true})
+.then(resp => console.log('mongo db connected...', resp))
+.catch(err => console.log('error=>', err));
+
 
 // Routes
 app.use('/', require('./routes/index'));
--- routes/users.js
+++ routes/users.js
@@ -4,16 +4,47 @@ const router = express.Router();
 router.get('/login', (req, res) => {
   // res.send('login');
   res.render('login');
-})
+});
 
 router.get('/register', (req, res) => {
   // res.send('Register');
   res.render('register');
-})
+});
+
+router.post('/register', (req, res) => {
+  console.log(req)
+  const { name, email, password, password2 } = req.body;
+  const errors = [];
+
+  if(!name || !email || !password || !password2){
+    errors.push({message: 'Please fill all the fields'});
+  }
+
+  if(password != password2){
+    errors.push({message: 'Password donot match'});
+  }
+
+  if(password.length < 6){
+    errors.push({message: 'Password should be atleast 6 charecters'});
+  }
+
+  if(errors.length > 0){
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    })
+  }else{
+    res.send('pass');
+  }
+
+});
 
 router.get('/dashboard', (req, res) => {
   // res.send('Register');
   res.render('dashboard');
-})
+});
 
 module.exports = router;
